2568faa269e17e5f7e486eb9ae425fde1e8fcddb
Tyler Martins <martinstyler14@gmail.com>
2023-12-05 20:37:14 -0500

    Section 9: Back-End to Front-End Routing in Production pt.4

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,5 +1,5 @@
 const keys = require("../config/keys");
-console.log(keys.stripeSecretKey);
+console.log("Stripe Secret Key: ", keys.stripeSecretKey);
 const stripe = require("stripe")(keys.stripeSecretKey);
 const requireLogin = require("../middlewares/requireLogin");
 
